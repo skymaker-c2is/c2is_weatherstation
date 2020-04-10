89590ca71726ac20e627e155feae8fb6c17648ac
Skymaker <lukas@skymaker.info>
2020-04-10 15:16:36 +0200

    Initial commit.

---
+++ c2is_weatherstation.ino
@@ -0,0 +1,13 @@
+#include <FS.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266httpUpdate.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
